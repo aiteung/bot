aca383d26f998ca8b587095d59fd6a4954d12e4f
Rolly Maulana Awangga <awangga@gmail.com>
2024-04-03 05:38:18 +0700

    stemmer

--- prepro.go
+++ prepro.go
@@ -2,7 +2,9 @@ package brain
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -34,4 +36,75 @@ func normalizeSentence(sentence string, punctRe *regexp.Regexp) string {
 	// Add more replacements and regex substitutions as needed
 	sentence = strings.TrimSpace(sentence)
 	return sentence
+}
+
+type Tokenizer struct {
+	// Example fields
+	Tokens map[string]int
+	// Add other fields as necessary
+}
+
+// LoadTokenizer loads a tokenizer from a file in JSON format.
+func LoadTokenizer(basePath, tokenizerPath string) (*Tokenizer, error) {
+	filePath := filepath.Join(basePath, tokenizerPath)
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var tokenizer Tokenizer
+	if err := json.Unmarshal(data, &tokenizer); err != nil {
+		return nil, err
+	}
+
+	return &tokenizer, nil
+}
+
+type Stemmer struct{}
+
+// stringJoin joins strings with a separator, similar to strings.Join
+func stringJoin(separator string, elements []string) string {
+	result := ""
+	for i, el := range elements {
+		if i > 0 {
+			result += separator
+		}
+		result += el
+	}
+	return result
+}
+
+// Stem reduces a word to its base form
+func (s *Stemmer) Stem(word string) string {
+	// Define common prefixes and suffixes
+	prefixes := []string{"ber", "ter", "meng", "peng"}
+	suffixes := []string{"kan", "i", "an"}
+
+	// Compile regular expressions for prefixes and suffixes
+	prefixRe := regexp.MustCompile("^(?i)(" + stringJoin("|", prefixes) + ")")
+	suffixRe := regexp.MustCompile("(?i)(" + stringJoin("|", suffixes) + ")$")
+
+	// Remove common prefixes
+	word = prefixRe.ReplaceAllString(word, "")
+
+	// Remove common suffixes
+	word = suffixRe.ReplaceAllString(word, "")
+	return word
+}
+
+func NewStemmer() *Stemmer {
+	return &Stemmer{}
+}
+
+func setConfig(fileName string) (*Stemmer, *regexp.Regexp, []string, string) {
+	stemmer := NewStemmer()
+	punctReEscape := regexp.MustCompile(`[!"#$%&'()*+,\-./:;<=>?@[\\\]^_` + "`{|}~]")
+	unknowns := []string{"gak paham", "kurang ngerti", "I don't know"}
+	path := filepath.Join(fileName, "/")
+
+	return stemmer, punctReEscape, unknowns, path
 }
---
+++ test.go
@@ -0,0 +1,46 @@
+package brain
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	// Example of using loadConfig and normalizeSentence
+	configPath := "config.json"
+	config, err := loadConfig(configPath)
+	println(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Example usage of normalizeSentence
+	punctRe := regexp.MustCompile(`[\!"#$%&'()*+,./:;<=>?@\[\\\]^_` + "`{|}~]")
+	sentence := "Example sentence to normalize!"
+	normalized := normalizeSentence(sentence, punctRe)
+	println(normalized)
+
+	// Example usage
+	tokenizer, err := LoadTokenizer("/path/to", "tokenizer.json")
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// Use the tokenizer
+	// For example, print the tokenizer data
+	fmt.Printf("Loaded tokenizer: %+v\n", tokenizer)
+
+	fileName := "data"
+	stemmer, punctReEscape, unknowns, path := setConfig(fileName)
+	fmt.Println(stemmer, punctReEscape, unknowns, path)
+
+	stemmer = NewStemmer()
+	words := []string{"berjalan", "menggunakan", "pengetahuan", "terapkan"}
+
+	for _, word := range words {
+		stemmedWord := stemmer.Stem(word)
+		fmt.Printf("Original: %-12s Stemmed: %-12s\n", word, stemmedWord)
+	}
+}
